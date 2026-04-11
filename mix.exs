19e8b3604fa366f7180768dd2ab6044eac5372e6
Diego M. Vadell <diego.m.vadell@gmail.com>
2026-04-11 19:51:29 +0000

    Add credo, dialyzer and precommit alias

--- mix.exs
+++ mix.exs
@@ -6,6 +6,7 @@ defmodule BranchedLlm.MixProject do
       app: :branched_llm,
       version: "0.1.0",
       elixir: "~> 1.19",
+      aliases: aliases(),
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
@@ -21,12 +22,29 @@ defmodule BranchedLlm.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-     {:req_llm, "~> 1.0.0"},
-     {:jason, "~> 1.2"},
-     {:retry, "~> 0.18"},
-     {:ecto, "~> 3.10"}, # For UUID generation
-     {:telemetry, "~> 1.0"},
-     {:mox, "~> 1.0", only: :test}
+      {:req_llm, "~> 1.0.0"},
+      {:jason, "~> 1.2"},
+      {:retry, "~> 0.18"},
+      # For UUID generation
+      {:ecto, "~> 3.10"},
+      {:telemetry, "~> 1.0"},
+      {:mox, "~> 1.0", only: :test},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.13", only: [:dev, :test], runtime: false}
+    ]
+  end
+
+  defp aliases do
+    [
+      precommit: [
+        "compile --warnings-as-errors",
+        "deps.unlock --unused",
+        "format",
+        "credo --strict",
+        ~s(cmd sh -c "MIX_ENV=test mix dialyzer"),
+        ~s(cmd sh -c "MIX_ENV=test mix test --cover")
+      ]
     ]
   end
 end
